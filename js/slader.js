0277e6d7bfa91119a4d2b5e86e2fb0e0fe788128
MaryoryChavez <maryory.chavez@gmail.com>
2017-04-06 09:32:49 -0500

    solucion merge 1

--- js/slader.js
+++ js/slader.js
@@ -4,47 +4,54 @@ var currentSection = null;
 var currentSec = null;
 
 
-function init()
-{
-	currentSection = $('#section-1');
-	$('#btn-sect1').click(onClickBtnFirst);
-	$('#btn-sect2').click(onClickBtnSecond);
-	$('#btn-sect3').click(onClickBtnThree);
-	$('#btn-sect4').click(onClickBtnFour);
-	$('#btn-sect5').click(onClickBtnFive);
+function init() {
+    currentSection = $('#section-1');
+    $('#btn-sect1').click(onClickBtnFirst);
+    $('#btn-sect2').click(onClickBtnSecond);
+    $('#btn-sect3').click(onClickBtnThree);
+    $('#btn-sect4').click(onClickBtnFour);
+    $('#btn-sect5').click(onClickBtnFive);
 
 
-	//TweenMax.from($('#saludo h1'), 1, {marginBottom:'0px', ease:Elastic.easeOut});
+    //TweenMax.from($('#saludo h1'), 1, {marginBottom:'0px', ease:Elastic.easeOut});
 }
 
 function onClickBtnFirst() {
-	
-	gotoSection('section-1');
+
+    gotoSection('section-1');
 }
+
 function onClickBtnSecond() {
-	
-	gotoSection('section-2');
+
+    gotoSection('section-2');
 }
+
 function onClickBtnThree() {
-	
-	gotoSection('section-3');
+
+    gotoSection('section-3');
 }
+
 function onClickBtnFour() {
-	
-	gotoSection('section-4');
+
+    gotoSection('section-4');
 }
+
 function onClickBtnFive() {
-	
-	gotoSection('section-5');
+
+    gotoSection('section-5');
 }
 
-function gotoSection(_identificadorDeSeccion)
-{
-	currentSection.removeClass('visible');
-	var nextSection = $('#'+_identificadorDeSeccion);
-	console.log(nextSection);
-	nextSection.addClass('visible animated fadeInLeft');
+function gotoSection(_identificadorDeSeccion) {
+    currentSection.removeClass('visible');
+    var nextSection = $('#' + _identificadorDeSeccion);
+    nextSection.addClass('visible animated fadeInLeft');
 
-	//TweenMax.from(nextSection, 1, {scale:0.2, opacity:0, ease:Elastic.easeOut});
-	currentSection = nextSection;
+    //TweenMax.from(nextSection, 1, {scale:0.2, opacity:0, ease:Elastic.easeOut});
+    currentSection = nextSection;
 }
+
+$('.btn-cambio1').click(function(evt) {
+    currentSection.removeClass('visible');
+    console.log(currentSection);
+    evt.preventDefault();
+});
